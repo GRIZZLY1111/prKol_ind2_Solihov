f771843815b8479c874e546dbbe357de43750f09
GRIZZLY1111 <msolihov28@gmail.com>
2025-04-01 11:08:39 +0500

    Add files via upload

---
+++ PraktikaIND2_2/PraktikaIND2_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PraktikaIND2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "Студенты.txt";
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не найден!");
+                Console.ReadKey();
+                return;
+            }
+            Queue<Student> allStudents = new Queue<Student>();
+            // Чтение данных из файла
+            foreach (string line in File.ReadLines(filePath))
+            {
+                string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length < 7) continue;
+
+                var student = new Student
+                {
+                    LastName = parts[0],
+                    FirstName = parts[1],
+                    MiddleName = parts[2],
+                    GroupNumber = parts[3],
+                    Grades = new[] { int.Parse(parts[4]), int.Parse(parts[5]), int.Parse(parts[6]) }
+                };
+                allStudents.Enqueue(student);
+            }
+            // Разделение студентов на две группы с сохранением порядка
+            var successfulStudents = allStudents.Where(s => s.IsSessionPassed());
+            var otherStudents = allStudents.Where(s => !s.IsSessionPassed());
+            // Объединение последовательностей и вывод
+            var result = successfulStudents.Concat(otherStudents);
+            Console.WriteLine("Результат:");
+            foreach (var student in result)
+            {
+                Console.WriteLine(student);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PraktikaIND2_2/PraktikaIND2_2/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraktikaIND2_2
+{
+    class Student
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string GroupNumber { get; set; }
+        public int[] Grades { get; set; }
+        //Проверка на сдачу экзамена
+        public bool IsSessionPassed()
+        {
+            return Grades.All(grade => grade >= 3);
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} {MiddleName}, Группа: {GroupNumber}, Оценки: {string.Join(", ", Grades)}";
+        }
+    }
+}
